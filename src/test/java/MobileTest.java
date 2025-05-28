52a480fd70337fd7f0a340f53ebd7b3c9561a3f9
Dunkin <117956284+DunkinYeo@users.noreply.github.com>
2025-05-29 08:58:23 +0900

    SDK app Automation Setup. appium : deviceName appium : platformName appium : automationName appium : platformVersion appium : app
    Merge remote-tracking branch 'origin/main'
    
    SDK App Automation Setup

--- src/test/java/MobileTest.java
+++ src/test/java/MobileTest.java
@@ -1,5 +1,9 @@
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -21,61 +25,86 @@ public class MobileTest {
                     new URL("http://127.0.0.1:4723/wd/hub"), cap);
 
             // Click Battery, Manufacturer, model number, serial number, hardware version, software version, firmware version
-
+/*
             if (driver.findElement(AppiumBy.xpath("//android.widget.TextView[@resource-id=\"com.wellysis.spatchex.spatchextest:id/bluetooth_status_tv\"]")).isDisplayed()){
                 System.out.println("Disconnected!");
+
                 driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_connect\"]")).click();
             }
-
+*/
             Thread.sleep(7000);
-
-            driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id=\"com.wellysis.spatchex.spatchextest:id/edit_serial_number\"]")).sendKeys("610262");
+            // Type Serial number TC1
+            driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id=\"com.wellysis.spatchex.spatchextest:id/edit_serial_number\"]")).sendKeys("670225");
 
             Thread.sleep(3000);
-
+            // Click Connect TC2
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_connect\"]")).click();
 
             Thread.sleep(10000);
-
+            // Click Battery TC3
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_battery\"]")).click();
 
             Thread.sleep(7000);
-
+            // Click Manufacturer TC4
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_manufacturer\"]")).click();
 
             Thread.sleep(7000);
-
+            // click ModelNumber - TC5 Bug with 1.76 FW?
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_model_number\"]")).click();
 
             Thread.sleep(7000);
-
+            // Click SerialNumber TC6
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_serial_number\"]")).click();
 
             Thread.sleep(7000);
-
+            // Click HardWareVersion TC7
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_hardware_version\"]")).click();
 
             Thread.sleep(5000);
-
+            // Click SoftwareVersion TC8
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_software_version\"]")).click();
 
             Thread.sleep(5000);
-
+            // Click FirmWareVersion TC9
             driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_firmware_version\"]")).click();
 
             Thread.sleep(5000);
+//Swipe up
+            private static void swipeUp(AndroidDriver driver) {
+                Dimension size = driver.manage().window().getSize();
+                int startX = size.width / 2;
+                int startY = (int) (size.height * 0.8);
+                int endY = (int) (size.height * 0.3);
 
+                new TouchAction<>(driver)
+                        .press(PointOption.point(startX, startY))
+                        .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+                        .moveTo(PointOption.point(startX, endY))
+                        .release()
+                        .perform();
+            }
             // ECG Live - Start measurement, end measurement, pause measurement, restart measurement.
-/*
-            driver.findElement(AppiumBy.xpath("digit_2")).click();
-            driver.findElement(AppiumBy.xpath("digit_2")).click();
+            driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_unLock\"]")).click();
 
+            Thread.sleep(5000);
+
+
+            driver.findElement(AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.wellysis.spatchex.spatchextest:id/btn_start_measurement\"]")).click();
+
+            Thread.sleep(5000);
+
+            //driver.findElement(AppiumBy.xpath("digit_2")).click();
+
+            //Thread.sleep(7000);
+
+
+/*
             // Set duration measurement, get duration measurement
 
             driver.findElement(AppiumBy.xpath("digit_2")).click();
             driver.findElement(AppiumBy.xpath("digit_2")).click();
 
-            // Memory - Get package number from memory, ECG Full, Acc, Gyro
+            // Memory - Get packet number from memory, ECG Full, Acc, Gyro
 
             driver.findElement(AppiumBy.xpath("digit_2")).click();
             driver.findElement(AppiumBy.xpath("digit_2")).click();
@@ -84,8 +113,8 @@ public class MobileTest {
 
             driver.findElement(AppiumBy.xpath("digit_2")).click();
             driver.findElement(AppiumBy.xpath("digit_2")).click();
-*/
 
+*/
 
             driver.quit();
         } catch (Exception e) {
